95f6206aa864b1388573790efd4a90bab471a01e
Shivam Chourasia <shivamchourasia15@gmail.com>
2022-01-11 20:48:56 +0530

    fixed

---
+++ src/main/java/PowerFinder.java
@@ -0,0 +1,10 @@
+
+public class PowerFinder {
+    public static int powerOf(int num, int toPower) {
+        int numRaisedToPower = 1;
+        for (int i = 0; i < toPower; i++) {
+            numRaisedToPower *= num;
+        }
+        return numRaisedToPower;
+    }
+}
